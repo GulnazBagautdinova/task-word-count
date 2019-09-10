ef93cab73cd573e0231f0dccd104fbe3ac4d2155
Венера <venera@MacBook-Pro-Venera.local>
2019-09-10 20:10:20 +0300

    format

--- main.go
+++ main.go
@@ -16,11 +16,10 @@ type result struct{
 	url string
 	count int
 	err error
-	
 }
 
 
-func search (url string) result {    
+func search (url string) result {
 
 	var res result
 	if url == "" {
@@ -33,7 +32,7 @@ func search (url string) result {
 		log.Fatal(err)
 	}
 	defer resp.Body.Close()
-	
+
 	if resp.StatusCode == http.StatusOK {
 		bodyBytes, err := ioutil.ReadAll(resp.Body)
 		if err != nil {
@@ -44,71 +43,55 @@ func search (url string) result {
 		// Check the page for the search term.
 		reg := regexp.MustCompile(`Go`)
 		matched := reg.FindAllString(bodyString,-1)
-        countmatched := len(matched)
+		countmatched := len(matched)
 		res = result{
 			url: url,
 			count: countmatched,
 		}
 	}
-		return res
+	return res
 }
 
 func main() {
-	var f *os.File	
+	var f *os.File
 	f = os.Stdin
 	defer f.Close()
-	//var urls []string		
+	//var urls []string
 	results := make(chan result)
 	doneChan := make(chan struct{})
-    
-	  go func (results <- chan result, doneChan chan <- struct {}) {
+
+	go func (results <- chan result, doneChan chan <- struct {}) {
 		var total int
-         for  res := range results{
-			 fmt.Printf ("Count for %s  - %d\n" , res.url, res.count )
-			 total +=  res.count
-		 }    
-		 fmt.Printf("Total count: %d\n", total)
-		 doneChan <- struct{}{}
-	  } (results, doneChan)
+		for  res := range results{
+			fmt.Printf ("Count for %s  - %d\n" , res.url, res.count )
+			total +=  res.count
+		}
+		fmt.Printf("Total count: %d\n", total)
+		doneChan <- struct{}{}
+	} (results, doneChan)
 
 	// Setup a wait group so we can process all the feeds.
 	var wg sync.WaitGroup
-	var url string	
-	
+	var url string
+
 	goroutines := make(chan struct{}, 5)
 	scanner := bufio.NewScanner(f)
-	for scanner.Scan() {	
-	url = scanner.Text()
-  // Set the number of goroutines we need to wait for while
-<<<<<<< HEAD
-<<<<<<< HEAD
-    goroutines  <- struct{}{}
-=======
-       goroutines  <- struct{}{}
->>>>>>> f9df75b2c14a9fa62bd88e551a26d4cc20738d29
-=======
-       goroutines  <- struct{}{}
->>>>>>> f9df75b2c14a9fa62bd88e551a26d4cc20738d29
-       wg.Add(1) 
-     go func (url string , results chan <- result, goroutines <- chan struct{}, wg *sync.WaitGroup) {
-	    results <- search (url)   
-	    <- goroutines
-	    wg.Done() 
-       }(url , results, goroutines, &wg)
-	 }
+	for scanner.Scan() {
+		url = scanner.Text()
+		// Set the number of goroutines we need to wait for while
+		goroutines  <- struct{}{}
+		wg.Add(1)
+		go func (url string , results chan <- result, goroutines <- chan struct{}, wg *sync.WaitGroup) {
+			results <- search (url)
+			<- goroutines
+			wg.Done()
+		}(url , results, goroutines, &wg)
+	}
 
-// Launch a goroutine to monitor when all the work is done.
+	// Launch a goroutine to monitor when all the work is done.
 	// Wait for everything to be processed.
 	wg.Wait()
 	close(goroutines)
 	close(results)
-    <- doneChan
-<<<<<<< HEAD
-<<<<<<< HEAD
+	<- doneChan
 }
-=======
-}
->>>>>>> f9df75b2c14a9fa62bd88e551a26d4cc20738d29
-=======
-}
->>>>>>> f9df75b2c14a9fa62bd88e551a26d4cc20738d29
